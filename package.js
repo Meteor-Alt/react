3a53bce843f2ff8646b14d8064eb1fd2a509698f
Edward <emmostrom@users.noreply.github.com>
2016-04-13 20:25:04 -0500

    remove react-meteor-data dependency

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'alt:react',
-  version: '1.0.2',
-  summary: 'Exports React and ReactDOM from either Meteor React or NPM React',
+  version: '1.0.3',
+  summary: 'Exports React, ReactDOM and ReactMeteorData from either Meteor React or NPM React',
   git: 'https://github.com/Meteor-Alt/react-packages',
   documentation: 'README.md'
 })
@@ -10,9 +10,8 @@ Package.onUse(function(api) {
   api.versionsFrom('1.2.1')
   api.use('ecmascript')
   api.use('react@0.14.3', {weak: true})
-  api.imply('react-meteor-data@0.1.4')
   api.imply('jsx@0.2.3')
   api.addFiles('react.js')
-  api.export(['React', 'ReactDOM'])
+  api.export(['React', 'ReactDOM', 'ReactMeteorData'])
 })
 
